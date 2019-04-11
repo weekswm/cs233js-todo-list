f67155a9c3d1914e251fab35a01830520a04852b
weekswm <weekswm@gmail.com>
2019-04-11 12:30:49 -0700

    part one done in class. ToDoList class complete.

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,42 @@
+class ToDoList {
+    constructor() {
+        this.tasks = [
+            {task: 'Go to Dentist', isComplete: false},
+            {task: 'Do Gardening', isComplete: true},
+            {task: 'Renew Library Account', isComplete: false}
+        ];
+        this.loadTasks = this.loadTasks.bind(this);
+        this.loadTasks();
+    }
+
+    generateTaskHtml(task, index) {
+        return `
+          <li class="list-group-item checkbox">
+            <div class="row">
+              <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox">
+                <label><input id="toggleTaskStatus" type="checkbox" value="" class="" 
+                    ${task.isComplete ? "checked" : ""}>
+                </label>
+              </div>
+              <div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 task-text ${task.isComplete ? "complete" : ""}">
+                ${task.task}
+              </div>
+              <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 delete-icon-area">
+                <a class="" href="/" ><i id="deleteTask" class="delete-icon glyphicon glyphicon-trash"></i></a>
+              </div>
+            </div>
+          </li>
+        `;
+    }
+
+    loadTasks() {
+        let tasksHtml = this.tasks.reduce(
+            (html, task, index) => html += this.generateTaskHtml(task, index), 
+            '');
+        document.getElementById("taskList").innerHTML = tasksHtml;
+    }
+}
+
 /*  Create a class called ToDoList
     PART 1 - Show the tasks
     -   Add a constructor
@@ -85,6 +124,8 @@
     Add a window on load event handler that instantiates a ToDo object.  
     Use and arrow or anonymous function
 */
+let toDo;
+window.onload = () => {toDo = new ToDoList();}
 
 function generateTaskHtml(task, index) {
     return `
